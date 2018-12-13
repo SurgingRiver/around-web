13520a747ad0d07051c1676eb1cf8fff3cbd2575
Huijun Cui <huijuncui@Huijuns-MacBook-Pro.local>
2018-12-12 23:03:16 -0500

    init

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { TopBar } from './TopBar';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <TopBar />
+            </div>
+        );
+    }
+}
+
+export default App;
